be623e7fdf3e5e4f0ace0e22a7cfcce08c4a7aac
RyanMitchell8 <N00233039@iadt.ie>
2025-09-24 00:56:18 +0100

    Started testing within laravel getting it to create fake names numbers and more. I've made the product and supplier tests but need to improve some parts of code.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
+use App\Models\Supplier;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            //SupplierSeeder::class,
+            ProductSeeder::class,
+            //UserSeeder::class,
         ]);
     }
 }
